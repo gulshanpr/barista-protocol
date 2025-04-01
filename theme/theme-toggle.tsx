d8dfd4ec7eef54e266182f46a2bf7cf1452e225b
Aditya Phasu <adityaphasu919@gmail.com>
2025-04-02 02:09:14 +0530

    add utility functions and SVG icons for Ethereum and USDC, and implement scroll area component

--- theme/theme-toggle.tsx
+++ theme/theme-toggle.tsx
@@ -8,22 +8,18 @@ export default function ThemeToggle() {
 
   return (
     <button
-      type="button"
       className="cursor-pointer rounded-md border-2 border-coffee px-3 py-1.5 flex gap-2 items-center justify-center transition-colors font-bold hover:bg-coffee hover:text-cream dark:hover:bg-cream dark:hover:text-coffee dark:border-cream"
       title="Toggle theme"
       aria-label="Toggle theme"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      {theme === "dark" ? (
-        <>
-          <Sun className="size-5" />
-          <span>Creama</span>
-        </>
-      ) : (
-        <>
-          <Moon className="size-5" />
-          <span>Espresso</span>
-        </>
-      )}
+      <div className="items-center gap-2 hidden dark:flex ">
+        <Sun className="size-5" />
+        <span className="hidden md:block">Espresso</span>
+      </div>
+      <div className="flex items-center gap-2 dark:hidden">
+        <Moon className="size-5" />
+        <span className="hidden md:block">Espresso</span>
+      </div>
     </button>
   );
 }
